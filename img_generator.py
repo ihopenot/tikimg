6dcc1a9efe73f0f8a239e86c51a33bea0677bc1d
ihopenot <984430652@qq.com>
2023-09-19 02:48:50 +0800

    support sdxl

--- img_generator.py
+++ img_generator.py
@@ -14,19 +14,25 @@ class ImgGenerator:
         assert r.status_code == 200
         return json.loads(r.text)
     
-    def set_model(self, info):
-        r = requests.post(f"{self.url}/options", json={"sd_model_checkpoint": info})
+    def set_model(self, info, isXL):
+        conf_json = {"sd_model_checkpoint": info}
+        if isXL:
+            conf_json["sd_vae"] = get_config()["xl_vae"]
+        else:
+            conf_json["sd_vae"] = get_config()["default_vae"]
+        r = requests.post(f"{self.url}/options", json=conf_json)
         assert r.status_code == 200
 
     def set_random_model(self):
         models = self.get_models()
         idx = random.randint(0, len(models)-1)
         name = models[idx]["title"]
-        self.set_model(name)
+        isXL = models[idx]["model_name"] in get_config()["sdxl_models"]
+        self.set_model(name, isXL)
 
         args = {"sd_model_checkpoint": name} 
 
-        return args
+        return args, isXL
     
     def get_img(self, args):
         r = requests.post(f"{self.url}/txt2img", json=args)
@@ -36,8 +42,11 @@ class ImgGenerator:
 
     def get_random_img(self):
         reload_config()
-        overwrite_args = self.set_random_model()
-        args = get_config()["default_args"]
+        overwrite_args, isXL = self.set_random_model()
+        if isXL:
+            args = get_config()["xl_default_args"]
+        else:
+            args = get_config()["default_args"]
 
         for k in overwrite_args:
             args[k] = overwrite_args[k]
